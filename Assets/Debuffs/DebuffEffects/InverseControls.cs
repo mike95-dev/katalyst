93f059426ff6a3aa501faeababaf59d01616b586
mason griffith <masongriffith22@gmail.com>
2024-11-08 12:41:08 -0700

    cleaned up things

--- Assets/Debuffs/DebuffEffects/InverseControls.cs
+++ Assets/Debuffs/DebuffEffects/InverseControls.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InverseControls : MonoBehavior 
+public class InverseControls : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if ()
-        //{
 
-        //}
+    }
+    public void ifTriggered()
+    {
+
     }
 }
