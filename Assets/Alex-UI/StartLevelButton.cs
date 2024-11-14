21229ffd81e9ae421e419c50ee287565638cd721
BlueDogXL <bluedogxl@gmail.com>
2024-11-13 17:16:10 -0700

    initial concepts

---
+++ Assets/Alex-UI/StartLevelButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevelButton : MonoBehaviour
+{
+    public void StartLevel()
+    {
+        // insert logic to start the level
+    }
+}
