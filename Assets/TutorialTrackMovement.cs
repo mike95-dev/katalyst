6f67fcbd68da52cb8305aa28821bbc230ac65473
LAUREN HESSELGESSER <lhesselg@gmail.com>
2025-01-26 19:32:54 -0800

    Revert "Implement HP system to player"
    This reverts commit 1410fdb6349457c6d75f78553aa61e0df5c41025.

--- Assets/TutorialTrackMovement.cs
+++ Assets/TutorialTrackMovement.cs
@@ -101,7 +101,7 @@ public class TutorialTrackMovement : MonoBehaviour
         transform.position = targetPosition;
     }
 
-    IEnumerator GameOverHandler()
+    IEnumerator CollisionHandler()
     {
         // Wait for 5 seconds
         yield return new WaitForSeconds(3f);
@@ -208,17 +208,6 @@ public class TutorialTrackMovement : MonoBehaviour
             Debug.LogError("No shootingscript component found on the 'Player' object!");
         }
 
-        HealthComponent healthComponent = player.GetComponent<HealthComponent>();
-
-        if (healthComponent != null)
-        {
-            healthComponent.currentHealth = healthComponent.maxHealth;
-        }
-        else
-        {
-            Debug.LogError("No shootingscript component found on the 'Player' object!");
-        }
-
-        StartCoroutine(GameOverHandler());
+        StartCoroutine(CollisionHandler());
     }
 }
